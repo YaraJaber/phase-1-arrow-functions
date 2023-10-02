8fa735fa763e8e708b5bcfc6bb4b7a4430e3fd06
YaraJaber <145011720+YaraJaber@users.noreply.github.com>
2023-10-02 14:53:13 +0400

    Completed assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+const divide = function(x, y) {
+    return x / y;
+  };
+  
+  const result = divide(2000, 100);
+  console.log(result);
+  
+  const square = (x) => x * x;
+  
+  console.log(square(4)); 
+  
+  const add = (x, y) => x + y;
+  
+  console.log(add(4, 3));
